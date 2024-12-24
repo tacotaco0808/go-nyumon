3c690cce561d834eafd4edae04f647950d1b67cf
Àżu <yamane_kota@techbowl.co.jp>
2024-12-25 02:16:28 +0900

    feat: station02

--- stations/station02/main.go
+++ stations/station02/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Station02")
+	x, y := 1, 2
+
+	fmt.Println("x: ", x, "y: ", y) // 期待される出力: 「x: 1, y: 2」
+
+	Swap(&x, &y)
+
+	fmt.Println("x: ", x, "y: ", y) // 期待される出力: 「x: 2, y: 1」
 }
+
+func Swap() {}
