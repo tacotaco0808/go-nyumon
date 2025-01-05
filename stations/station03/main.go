fd71ce7c3c5d9f274f625344b6529dfe37862fef
Àżu <yamane_kota@techbowl.co.jp>
2025-01-05 18:07:18 +0900

    feat: station03

--- stations/station03/main.go
+++ stations/station03/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Station03")
+	books := []Book{} // 値は自由に定義してください
+
+	totalPrice := Total(books)
+	fmt.Println("合計", totalPrice, "円")
+
+	authorBooksMap := ToMap(books)
+	authorName := ""
+	fmt.Println("著者: ", authorName, ", タイトル: ", authorBooksMap[authorName].Title)
 }
+
+// 構造体 Book を定義 ---
+
+// フィールド
+// Title ... 文字列、本のタイトル
+// Author ... 文字列、著者
+// Price ... 整数、価格
+// Categories ... 文字列のスライス、カテゴリ
+
+// --------------------
+
+// Bookの配列を引数とし、本の合計価格を戻り値とする関数 `Total` を実装する
+func Total() {}
+
+// 3. キーを「著者名 (Author)」、値を構造体 Book とするマップを戻り値とする関数 `ToMap` を実装する
+func ToMap() {}
--- stations/station03/main_test.go
+++ stations/station03/main_test.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStation03(t *testing.T) {
+	t.Run("構造体Bookを正しく定義できている", func(t *testing.T) {
+		book := Book{
+			Title:      "タイトル",
+			Author:     "著者",
+			Price:      0,
+			Categories: []string{"カテゴリ1", "カテゴリ2"},
+		}
+
+		require.NotNil(t, book)
+	})
+
+	t.Run("Total関数を正しく実装できている", func(t *testing.T) {
+		books := []Book{
+			{
+				Price: 100,
+			},
+			{
+				Price: 200,
+			},
+			{
+				Price: 300,
+			},
+		}
+
+		totalPrice := Total(books)
+		expected := 600
+
+		require.Equal(t, expected, totalPrice)
+	})
+
+	t.Run("ToMap関数を正しく実装できている", func(t *testing.T) {
+		books := []Book{
+			{
+				Title:  "A",
+				Author: "田中 太郎",
+			},
+			{
+				Title:  "B",
+				Author: "佐藤 花子",
+			},
+			{
+				Title:  "C",
+				Author: "鈴木 次郎",
+			},
+		}
+
+		authorBooksMap := ToMap(books)
+
+		require.Equal(t, "A", authorBooksMap["田中 太郎"].Title)
+		require.Equal(t, "B", authorBooksMap["佐藤 花子"].Title)
+		require.Equal(t, "C", authorBooksMap["鈴木 次郎"].Title)
+	})
+}
