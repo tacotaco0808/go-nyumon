93eff2f2c06da2afa904adcfbf85c37eb4d37481
Àżu <yamane_kota@techbowl.co.jp>
2025-01-10 21:19:15 +0900

    feat: station04

--- stations/station04/main.go
+++ stations/station04/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Station04")
+	rectangle := Rectangle{
+		Width:  30,
+		Height: 15.5,
+	}
+
+	circle := Circle{
+		Radius: 10,
+	}
+
+	printArea(rectangle)
+	printArea(circle)
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func printArea(s Shape) {
+	fmt.Println("面積: ", s.Area(), "㎠")
 }
