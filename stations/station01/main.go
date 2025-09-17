7930639caffc20b38230f6f56cc0270e954d5de3
tacotaco0808 <takuto1488@gmail.com>
2025-09-17 15:33:44 +0900

    station1

--- stations/station01/main.go
+++ stations/station01/main.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+func FizzBuzz (){
+	for i:=1;i<=100;i++{
+		switch {
+			case i%15==0:
+				fmt.Println("FizzBuzz")
+			case i%3==0:
+				fmt.Println("Fizz")
+			case i%5==0:
+				fmt.Println("Buzz")
+			default:
+				fmt.Println(i)
+		}
+	}
+}
 func main() {
 	fmt.Println("Start FizzBuzz....")
-
+	
 	FizzBuzz() // この関数を実装する
 
 	fmt.Println("FizzBuzz completed!")
