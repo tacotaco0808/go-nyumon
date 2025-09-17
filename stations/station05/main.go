c0ec88a2b6a60b5ab1ae5b9952c53edc1e27a459
tacotaco0808 <takuto1488@gmail.com>
2025-09-17 23:06:53 +0900

    station5

--- stations/station05/main.go
+++ stations/station05/main.go
@@ -1,11 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
 func main() {
-	sampleFile := "./sample.txt"
+	sampleFile := "sample.txt"
 	CheckFileExist(sampleFile)
 
 	notExistFile := "./abc.txt"
 	CheckFileExist(notExistFile)
 }
 
-func CheckFileExist(path string) {}
+func CheckFileExist(path string) {
+	if _, err := os.Stat(path); err == nil {
+        fmt.Printf("ファイルが見つかりました")
+    } else {
+        fmt.Printf("ファイルが見つかりませんでした")
+    }
+}
